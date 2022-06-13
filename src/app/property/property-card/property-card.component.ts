b1ae8d6cb764534a9603897cae119d815ee231e9
YuraRov <yura.rov555@gmail.com>
2022-06-13 21:30:55 +0300

    Learned about *ngFor and Input decorator

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-property-card',
@@ -7,10 +7,6 @@ import { Component } from "@angular/core";
 }
 )
 export class PropertyCardComponent {
-  Property: any = {
-    "Id": 1,
-    "Name": "Lovely house",
-    "Type": "House",
-    "Price": 12000
-  }
+  @Input() property: any;
+
 }
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -3,13 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
+  properties: Array<any> = [
+    {
+      Id: 1,
+      Name: 'Lovely house',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 2,
+      Name: 'Not bad flat',
+      Type: 'Flat',
+      Price: 6000,
+    },
+    {
+      Id: 3,
+      Name: 'Cool villa',
+      Type: 'House',
+      Price: 20000,
+    },
+    {
+      Id: 4,
+      Name: 'Expensive house',
+      Type: 'House',
+      Price: 20000,
+    },
+    {
+      Id: 5,
+      Name: 'Cheap house',
+      Type: 'House',
+      Price: 3000,
+    },
+    {
+      Id: 6,
+      Name: 'Cool house',
+      Type: 'House',
+      Price: 50000,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
